4be5862355dd97ddf234311adaa1b7a902ce9ec0
Izan Rodrigues <izanrodrigues999@gmail.com>
2023-08-05 09:10:00 -0300

    updating framework error handling

--- duvua-framework/src/errors.rs
+++ duvua-framework/src/errors.rs
@@ -1,7 +1,10 @@
 use crate::builder::interaction_response::InteractionResponse;
 use serenity::{
     builder::{CreateComponents, CreateInteractionResponseData},
-    model::prelude::{application_command::ApplicationCommandInteraction, InteractionResponseType},
+    model::prelude::{
+        application_command::ApplicationCommandInteraction,
+        message_component::MessageComponentInteraction, InteractionResponseType,
+    },
     prelude::Context,
 };
 
@@ -22,7 +25,41 @@ impl BotError {
         ""
     }
 
-    pub async fn respond_to(
+    #[inline]
+    pub fn get_response(&self, defered: bool) -> InteractionResponse<'_> {
+        let mut response = InteractionResponse::default();
+        response
+            .set_kind(if defered {
+                InteractionResponseType::UpdateMessage
+            } else {
+                InteractionResponseType::ChannelMessageWithSource
+            })
+            .set_data(
+                CreateInteractionResponseData::default()
+                    .set_components(CreateComponents::default())
+                    .set_embeds(Vec::new())
+                    .content(self.get_message()),
+            );
+
+        response
+    }
+
+    pub async fn respond_message_component(
+        &self,
+        ctx: &Context,
+        interaction: &MessageComponentInteraction,
+    ) {
+        if let &BotError::Serenity(e) = &self {
+            log::error!("Serenity error: {e}");
+        } else {
+            _ = self
+                .get_response(false)
+                .respond_message_component(ctx.http.as_ref(), interaction)
+                .await;
+        }
+    }
+
+    pub async fn respond_application_command(
         &self,
         ctx: &Context,
         interaction: &ApplicationCommandInteraction,
@@ -31,19 +68,9 @@ impl BotError {
         if let &BotError::Serenity(e) = &self {
             log::error!("Serenity error: {e}");
         } else {
-            _ = InteractionResponse::default()
-                .set_kind(if defered {
-                    InteractionResponseType::UpdateMessage
-                } else {
-                    InteractionResponseType::ChannelMessageWithSource
-                })
-                .set_data(
-                    CreateInteractionResponseData::default()
-                        .set_components(CreateComponents::default())
-                        .set_embeds(Vec::new())
-                        .content(self.get_message()),
-                )
-                .respond_message_component(ctx.http.as_ref(), interaction)
+            _ = self
+                .get_response(defered)
+                .respond_application_command(ctx.http.as_ref(), interaction)
                 .await;
         }
     }
