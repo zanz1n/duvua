8d4e7519a02319df28688ffd6f63bbad4f2e01b1
Izan Rodrigues <izanrodrigues999@gmail.com>
2023-08-15 19:29:08 -0300

    refactoring

--- services/chimera/src/handlers/component_handler.rs
+++ services/chimera/src/handlers/component_handler.rs
@@ -31,6 +31,63 @@ impl MessageComponentHandler {
             cache,
         }
     }
+
+    async fn kiss_handler(
+        &self,
+        ctx: &Context,
+        interaction: &MessageComponentInteraction,
+        id_prefix: &str,
+        id_rest: &str,
+    ) -> Result<(), BotError> {
+        if id_rest.len() != 30 {
+            return Ok(());
+        }
+
+        let info: Option<KissCacheData> =
+            self.cache.de_get("component/".to_owned() + id_rest).await?;
+        let info = match info {
+            Some(v) => v,
+            None => {
+                return InteractionResponse::default()
+                    .set_kind(InteractionResponseType::UpdateMessage)
+                    .set_data(
+                        CreateInteractionResponseData::default()
+                            .set_components(
+                                CreateActionRow::default()
+                                    .add_button(create_kiss_reply_button(SKIP_STR, false))
+                                    .add_button(create_kiss_deny_button(SKIP_STR, false))
+                                    .to_components(),
+                            )
+                            .to_owned(),
+                    )
+                    .respond_message_component(&ctx.http, interaction)
+                    .await
+            }
+        };
+
+        if info.target_id != interaction.user.id.0 {
+            return InteractionResponse::with_content_ephemeral("Isso não é pra você enxerido!")
+                .respond(&ctx.http, interaction.id.0, &interaction.token)
+                .await;
+        }
+
+        let reponse = if id_prefix == "rkiss" {
+            self.shared_handler
+                .handle_kiss_reply(info.user_id, info.target_id)
+                .await?
+        } else {
+            self.shared_handler
+                .handle_kiss_deny(info.user_id, info.target_id)
+                .await?
+        };
+
+        self.cache.del("component/".to_owned() + id_rest).await?;
+        expiry_kiss_buttons(&ctx.http, &info.interaction_token).await?;
+
+        reponse
+            .respond(&ctx.http, interaction.id.0, &interaction.token)
+            .await
+    }
 }
 
 #[async_trait]
@@ -49,57 +106,7 @@ impl CommandHandler for MessageComponentHandler {
         let prefix = &custom_id[0..5];
 
         if prefix == "rkiss" || prefix == "dkiss" {
-            let rest = &custom_id[6..];
-
-            if rest.len() != 30 {
-                return Ok(());
-            }
-
-            let info: Option<KissCacheData> =
-                self.cache.de_get("component/".to_owned() + rest).await?;
-            let info = match info {
-                Some(v) => v,
-                None => {
-                    return InteractionResponse::default()
-                        .set_kind(InteractionResponseType::UpdateMessage)
-                        .set_data(
-                            CreateInteractionResponseData::default()
-                                .set_components(
-                                    CreateActionRow::default()
-                                        .add_button(create_kiss_reply_button(SKIP_STR, false))
-                                        .add_button(create_kiss_deny_button(SKIP_STR, false))
-                                        .to_components(),
-                                )
-                                .to_owned(),
-                        )
-                        .respond_message_component(&ctx.http, interaction)
-                        .await
-                }
-            };
-
-            if info.target_id != interaction.user.id.0 {
-                return InteractionResponse::with_content_ephemeral(
-                    "Isso não é pra você enxerido!",
-                )
-                .respond(&ctx.http, interaction.id.0, &interaction.token)
-                .await;
-            }
-
-            let reponse = if prefix == "rkiss" {
-                self.shared_handler
-                    .handle_kiss_reply(info.user_id, info.target_id)
-                    .await?
-            } else {
-                self.shared_handler
-                    .handle_kiss_deny(info.user_id, info.target_id)
-                    .await?
-            };
-
-            self.cache.del("component/".to_owned() + rest).await?;
-            expiry_kiss_buttons(&ctx.http, &info.interaction_token).await?;
-
-            reponse
-                .respond(&ctx.http, interaction.id.0, &interaction.token)
+            self.kiss_handler(ctx, interaction, prefix, &custom_id[6..])
                 .await
         } else {
             Ok(())
