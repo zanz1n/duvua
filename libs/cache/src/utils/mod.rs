3fb0bb0d1f32108e4a28033857ac319893caa04a
Izan Rodrigues <izanrodrigues999@gmail.com>
2023-09-01 19:11:56 -0300

    adding guild cache utility

--- libs/cache/src/utils/mod.rs
+++ libs/cache/src/utils/mod.rs
@@ -1,5 +1,8 @@
 use duvua_framework::errors::BotError;
-use serenity::{http::Http, model::user::User};
+use serenity::{
+    http::Http,
+    model::{prelude::PartialGuild, user::User},
+};
 
 use crate::{redis::RedisCacheService, CacheRepository};
 
@@ -10,7 +13,7 @@ pub async fn get_or_store_user(
 ) -> Result<User, BotError> {
     let key = id.to_string();
 
-    if let Some(user) =  cache.as_ref().de_get(key.clone()).await? {
+    if let Some(user) = cache.as_ref().de_get(key.clone()).await? {
         return Ok(user);
     }
 
@@ -23,4 +26,26 @@ pub async fn get_or_store_user(
     cache.as_ref().ser_set_ttl(key, user.clone(), 60).await?;
 
     Ok(user)
+}
+
+pub async fn get_or_store_guild(
+    http: impl AsRef<Http>,
+    cache: impl AsRef<RedisCacheService>,
+    id: u64,
+) -> Result<PartialGuild, BotError> {
+    let key = id.to_string();
+
+    if let Some(guild) = cache.as_ref().de_get(key.clone()).await? {
+        return Ok(guild);
+    }
+
+    let guild = http
+        .as_ref()
+        .get_guild(id)
+        .await
+        .or_else(|e| Err(BotError::Serenity(e)))?;
+
+    cache.as_ref().ser_set_ttl(key, guild.clone(), 60).await?;
+
+    Ok(guild)
 }
