2e586293a51346f13ed4258c0ae5cd83b350ef2c
Izan Rodrigues <izanrodrigues999@gmail.com>
2024-02-24 18:37:18 -0300

    updating docker setup script

--- docker/setup.sh
+++ docker/setup.sh
@@ -5,7 +5,7 @@ docker-compose pull
 docker-compose build duvua_migrator
 docker-compose up -d duvua_postgresql
 sleep 3
-docker-compose run --rm -it duvua_migrator bash -c "cargo sqlx database create && cargo sqlx migrate run"
+docker-compose run --rm duvua_migrator bash -c "cargo sqlx database create && cargo sqlx migrate run"
 docker-compose down
 
 docker-compose build --parallel
