8118fe532b2e8d91d47c51207579e6b5b5044644
Izan Rodrigues <izanrodrigues999@gmail.com>
2024-08-01 06:04:37 -0300

    add runtime migrations

--- cmd/duvua-bot/main.go
+++ cmd/duvua-bot/main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"context"
+	"database/sql"
+	"flag"
 	"log"
 	"log/slog"
 	"os"
@@ -10,7 +12,11 @@ import (
 	"time"
 
 	"github.com/bwmarrin/discordgo"
+	gomigrate "github.com/golang-migrate/migrate/v4"
+	migratepgx "github.com/golang-migrate/migrate/v4/database/pgx/v5"
+	"github.com/golang-migrate/migrate/v4/source/iofs"
 	"github.com/jackc/pgx/v5/pgxpool"
+	"github.com/jackc/pgx/v5/stdlib"
 	"github.com/jackc/pgx/v5/tracelog"
 	"github.com/joho/godotenv"
 	"github.com/zanz1n/duvua-bot/config"
@@ -18,14 +24,27 @@ import (
 	"github.com/zanz1n/duvua-bot/internal/events"
 	"github.com/zanz1n/duvua-bot/internal/logger"
 	"github.com/zanz1n/duvua-bot/internal/manager"
+	embedsql "github.com/zanz1n/duvua-bot/sql"
+)
+
+var (
+	migrate = flag.Bool("migrate", false, "Migrates the database")
+	debug   = flag.Bool("debug", false, "Enables debug logs")
 )
 
 var endCh chan os.Signal
 
 func init() {
+	flag.Parse()
+}
+
+func init() {
 	godotenv.Load()
 
 	config := config.GetConfig()
+	if *debug {
+		config.LogLevel = slog.LevelDebug
+	}
 
 	slog.SetLogLoggerLevel(config.LogLevel)
 
@@ -34,35 +53,23 @@ func init() {
 }
 
 func main() {
-	config := config.GetConfig()
+	cfg := config.GetConfig()
 
-	s, err := discordgo.New("Bot " + config.Discord.Token)
+	s, err := discordgo.New("Bot " + cfg.Discord.Token)
 	if err != nil {
 		log.Fatalln("Failed to create discord session:", err)
 	}
 
-	s.LogLevel = logger.SlogLevelToDiscordgo(config.LogLevel + 4)
+	s.LogLevel = logger.SlogLevelToDiscordgo(cfg.LogLevel + 4)
 
-	pgxConfig, err := pgxpool.ParseConfig(config.Postgres.IntoUri())
-	if err != nil {
-		log.Fatalln("Failed to parse postgres config:", err)
+	db := connectToPostgres()
+	defer db.Close()
+
+	if *migrate {
+		if err = execMigration(db); err != nil {
+			log.Fatalln("Failed to run migrations:", err)
+		}
 	}
-
-	pgxConfig.ConnConfig.Tracer = &tracelog.TraceLog{
-		Logger:   logger.NewPgxLogger(slog.Default()),
-		LogLevel: logger.SlogLevelToPgx(config.LogLevel),
-	}
-	pgxConfig.MaxConns = config.Postgres.MaxConns
-	pgxConfig.MinConns = config.Postgres.MinConns
-
-	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-	defer cancel()
-
-	pool, err := pgxpool.NewWithConfig(ctx, pgxConfig)
-	if err != nil {
-		log.Fatalln("Failed to connect to postgres:", err)
-	}
-	defer pool.Close()
 
 	m := manager.NewManager()
 
@@ -80,4 +87,72 @@ func main() {
 
 	<-endCh
 	log.Println("Closing bot ...")
+}
+
+func connectToPostgres() *sql.DB {
+	cfg := config.GetConfig()
+
+	pgxConfig, err := pgxpool.ParseConfig(cfg.Postgres.IntoUri())
+	if err != nil {
+		log.Fatalln("Failed to parse postgres config:", err)
+	}
+
+	pgxConfig.ConnConfig.Tracer = &tracelog.TraceLog{
+		Logger:   logger.NewPgxLogger(slog.Default()),
+		LogLevel: logger.SlogLevelToPgx(cfg.LogLevel + 4),
+	}
+	pgxConfig.MaxConns = cfg.Postgres.MaxConns
+	pgxConfig.MinConns = cfg.Postgres.MinConns
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	dbStart := time.Now()
+	pool, err := pgxpool.NewWithConfig(ctx, pgxConfig)
+	if err != nil {
+		log.Fatalln("Failed to connect to postgres:", err)
+	}
+
+	slog.Info("Connected to database", "took", time.Since(dbStart))
+
+	db := stdlib.OpenDBFromPool(pool)
+
+	if err = db.Ping(); err != nil {
+		log.Fatalln("Failed to connect to postgres:", err)
+	}
+
+	return db
+}
+
+func execMigration(db *sql.DB) error {
+	start := time.Now()
+	cfg := config.GetConfig()
+
+	source, err := iofs.New(embedsql.Migrations, "migrations")
+	if err != nil {
+		return err
+	}
+
+	driver, err := migratepgx.WithInstance(db, &migratepgx.Config{
+		DatabaseName:     cfg.Postgres.Database,
+		StatementTimeout: 10 * time.Second,
+	})
+	if err != nil {
+		return err
+	}
+
+	migrator, err := gomigrate.NewWithInstance("iofs", source, "pgx5", driver)
+	if err != nil {
+		return err
+	}
+
+	if err = migrator.Up(); err != nil {
+		if err != gomigrate.ErrNoChange {
+			return err
+		}
+	}
+
+	slog.Info("Migrations were run", "took", time.Since(start))
+
+	return nil
 }
--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.22.5
 
 require (
 	github.com/bwmarrin/discordgo v0.28.1
+	github.com/golang-migrate/migrate/v4 v4.17.1
 	github.com/google/uuid v1.6.0
 	github.com/jackc/pgx/v5 v5.6.0
 	github.com/joho/godotenv v1.5.1
@@ -14,13 +15,18 @@ require (
 require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/gorilla/websocket v1.5.3 // indirect
+	github.com/hashicorp/errwrap v1.1.0 // indirect
+	github.com/hashicorp/go-multierror v1.1.1 // indirect
+	github.com/jackc/pgerrcode v0.0.0-20240316143900-6e2875d9b438 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
 	github.com/jackc/puddle/v2 v2.2.1 // indirect
 	github.com/kr/text v0.2.0 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/rogpeppe/go-internal v1.12.0 // indirect
+	go.uber.org/atomic v1.11.0 // indirect
 	golang.org/x/crypto v0.25.0 // indirect
+	golang.org/x/net v0.23.0 // indirect
 	golang.org/x/sync v0.7.0 // indirect
 	golang.org/x/sys v0.22.0 // indirect
 	golang.org/x/text v0.16.0 // indirect
