f4b233c0c8edcf80fdbe104488004cce7706f868
Izan Rodrigues <izanrodrigues999@gmail.com>
2024-08-10 05:45:28 -0300

    update: just some typo

--- sql/migrations/0001_add_welcome.up.sql
+++ sql/migrations/0001_add_welcome.up.sql
@@ -3,11 +3,11 @@
 CREATE TYPE welcometype AS ENUM ('Message', 'Image', 'Embed');
 
 CREATE TABLE welcome (
-    id BIGINT PRIMARY KEY,
-    created_at TIMESTAMPTZ(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMPTZ(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    enabled BOOLEAN NOT NULL DEFAULT FALSE,
-    channel_id BIGINT,
-    message VARCHAR(255) NOT NULL DEFAULT 'Seja Bem Vind@ ao servidor {{USER}}',
+    id bigint PRIMARY KEY,
+    created_at timestamptz(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at timestamptz(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    enabled boolean NOT NULL DEFAULT FALSE,
+    channel_id bigint,
+    message varchar(255) NOT NULL DEFAULT 'Seja Bem Vind@ ao servidor {{USER}}',
     kind welcometype NOT NULL DEFAULT 'Message'
 );
