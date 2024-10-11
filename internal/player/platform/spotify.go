17ed3b2c1ae31b9f15242de363a0a0b045be9272
Izan Rodrigues <izanrodrigues999@gmail.com>
2024-10-11 01:45:34 -0300

    update spotify client: add method for retrying in case of authentication error

--- internal/player/platform/spotify.go
+++ internal/player/platform/spotify.go
@@ -5,6 +5,8 @@ import (
 	"log/slog"
 	"net/url"
 	"strings"
+
+	"sync/atomic"
 	"time"
 
 	"github.com/zanz1n/duvua/internal/errors"
@@ -18,42 +20,43 @@ import (
 var _ Platform = &Spotify{}
 
 type Spotify struct {
-	c  *spotify.Client
+	c  atomic.Pointer[spotify.Client]
 	yt *Youtube
+
+	cfg clientcredentials.Config
 }
 
 func NewSpotify(clientId, clientSecret string, yt *Youtube) (*Spotify, error) {
-	config := &clientcredentials.Config{
-		ClientID:     clientId,
-		ClientSecret: clientSecret,
-		TokenURL:     spotifyauth.TokenURL,
-	}
-
-	token, err := config.Token(context.Background())
-	if err != nil {
-		return nil, err
-	}
-
-	hc := spotifyauth.New().Client(context.Background(), token)
-	client := spotify.New(hc)
-
-	return &Spotify{
-		c:  client,
+	s := &Spotify{
 		yt: yt,
-	}, nil
+		cfg: clientcredentials.Config{
+			ClientID:     clientId,
+			ClientSecret: clientSecret,
+			TokenURL:     spotifyauth.TokenURL,
+		},
+	}
+
+	if err := s.rollAuth(); err != nil {
+		return nil, errors.Unexpected("spotify auth: " + err.Error())
+	}
+
+	return s, nil
 }
 
 // SearchString implements Platform.
 func (s *Spotify) SearchString(query string) (*player.TrackData, error) {
-	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
-	defer cancel()
+	res, err := authRetry(s, func(c *spotify.Client) (*spotify.SearchResult, error) {
+		ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+		defer cancel()
 
-	res, err := s.c.Search(
-		ctx,
-		query,
-		spotify.SearchTypeTrack,
-		spotify.Limit(2),
-	)
+		return c.Search(
+			ctx,
+			query,
+			spotify.SearchTypeTrack,
+			spotify.Limit(2),
+		)
+	})
+
 	if err != nil {
 		return nil, errors.Unexpected(
 			"spotify search string: " + err.Error(),
@@ -85,12 +88,15 @@ func (s *Spotify) SearchUrl(uri string) ([]player.TrackData, error) {
 	id := pathS[len(pathS)-1]
 	pathS = pathS[:len(pathS)-1]
 
-	ctx, cancel := context.WithTimeout(context.Background(), 2*time.Second)
-	defer cancel()
-
 	switch pathS[1] {
 	case "track":
-		track, err := s.c.GetTrack(ctx, spotify.ID(id))
+		track, err := authRetry(s, func(c *spotify.Client) (*spotify.FullTrack, error) {
+			ctx, cancel := context.WithTimeout(context.Background(), 2*time.Second)
+			defer cancel()
+
+			return c.GetTrack(ctx, spotify.ID(id))
+		})
+
 		if err != nil {
 			return nil, errors.Unexpected(
 				"spotify search track: " + err.Error(),
@@ -123,6 +129,51 @@ func (s *Spotify) Fetch(query string) (Streamer, error) {
 	panic("must not be used")
 }
 
+func (s *Spotify) rollAuth() error {
+	start := time.Now()
+
+	token, err := s.cfg.Token(context.Background())
+	if err != nil {
+		slog.Error(
+			"Spotify: Failed to reroll auth token",
+			"took", time.Since(start).Round(time.Millisecond),
+			"error", err,
+		)
+		return err
+	}
+
+	hc := spotifyauth.New().Client(context.Background(), token)
+	client := spotify.New(hc)
+
+	s.c.Store(client)
+	slog.Info(
+		"Spotify: Rerolled auth token",
+		"took", time.Since(start).Round(time.Millisecond),
+	)
+
+	return nil
+}
+
+func authRetry[T any](s *Spotify, f func(c *spotify.Client) (*T, error)) (*T, error) {
+	if s.c.Load() == nil {
+		if err := s.rollAuth(); err != nil {
+			return nil, err
+		}
+	}
+
+	t, err := f(s.c.Load())
+
+	if err != nil && isExpiredTokenErr(err) {
+		if err2 := s.rollAuth(); err2 != nil {
+			return nil, err2
+		}
+
+		t, err = f(s.c.Load())
+	}
+
+	return t, err
+}
+
 func (s *Spotify) ytConvert(track *spotify.FullTrack) (*player.TrackData, error) {
 	ytSearchQuery := ""
 	if len(track.Artists) > 0 {
@@ -131,4 +182,8 @@ func (s *Spotify) ytConvert(track *spotify.FullTrack) (*player.TrackData, error)
 	ytSearchQuery += track.Name
 
 	return s.yt.SearchString(ytSearchQuery)
+}
+
+func isExpiredTokenErr(err error) bool {
+	return strings.Contains(err.Error(), "token expired")
 }
